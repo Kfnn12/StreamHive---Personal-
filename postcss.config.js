8c37e73930fcbb57b0966cc8e9a697ac53c50063
Kfnn12 <132144405+Kfnn12@users.noreply.github.com>
2024-01-20 19:03:16 +0800

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  // other rules...
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
   },
 }
